e0679ed8a9a3bc9497688ae1477ffe56ac85450d
Wiz-Omar <oyounes57@gmail.com>
2026-09-10 00:19:48 +0200

    added usercreat and userlogin pydantic schemas to user model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,20 @@
 from datetime import datetime
 
 from app.database import Base
+from pydantic import BaseModel
 from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.orm import relationship
 
+
+class UserCreate(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    password: str
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
 
 class User(Base):
     __tablename__ = 'users'
