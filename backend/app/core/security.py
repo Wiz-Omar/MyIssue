da8b1e8c6614e397760a348d32d7d4d445b63a22
Wiz-Omar <oyounes57@gmail.com>
2026-09-22 01:38:40 +0200

    fix typo in jwt decode parameter. algorithm -> algorithms

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -13,11 +13,11 @@ def encode_jwt_token(payload: dict):
     return jwt.encode(payload, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
 
 def decode_jwt_token(token: str):
-    return jwt.decode(token, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
+    return jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
 
 def verify_password(plain_password, hashed_password: str | None = None) -> bool:
-    # Calling verify with not hash_password allows burning the same time when no user is found
-    # Makes the response timing indistinguishable for an attacker
+    #calling verify with not hash_password allows burning the same time when no user is found
+    #makes the response timing indistinguishable for an attacker
     if not hashed_password:
         hashed_password = password_hash.hash("dummypassword")
     return password_hash.verify(plain_password, hashed_password)
